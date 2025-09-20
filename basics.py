969fec518608dbd3c929bd2d928127240db9a06d
Ianizmaname <iamnjuguna90@gmail.com>
2025-09-20 14:00:09 +0300

    added basics.py

---
+++ basics.py
@@ -0,0 +1,34 @@
+# # execution order
+# print('line 1') # this will be ignored
+# print('line 2')
+# print('line 3')
+
+# # datatypes
+# print('words')
+# print("words")
+# print(123) # integers / ints
+# print(-10)
+# print(-10)
+# print(1.5) # floating point value /float 
+
+# # operations
+# print(3 + 3)
+# print(3 - 3)
+# print(3 * 3)
+# print(3 / 3)
+# print(10 + 0.1234123412341234)
+# print(10 * 'hello')
+# print(13 - 3 * (4 + 2))
+
+# # variables
+# test_value = 123
+# test_value += 50
+# print(test_value + 10)
+
+# # input
+# input("Please write something ")
+# print(user_input)
+
+# exercise
+user_name = input('What is your name? ')
+print('hello',user_name, 'Have a nice day :)' )
