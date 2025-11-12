37bad2a9fc184101c077b0edb934a069468c0276
Ianizmaname <iamnjuguna90@gmail.com>
2025-11-12 08:49:45 +0300

    added graph.py

---
+++ graph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import emoji
+
+# data
+# x = [i for i in range(0,11)]
+# y = [y ** 2 for y in x]
+
+# plt.plot(x,y)
+
+# plt.xlabel('X axis')
+# plt.ylabel('something else')
+
+# plt.show()
+
+# exercise emojis
+print(emoji.emojize('Python is :thumbs_up:'))
